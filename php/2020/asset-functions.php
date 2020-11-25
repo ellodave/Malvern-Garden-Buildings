3a924ac4a11e9a556989a4fec6702feb96f89658
ellodave <34511073+ellorich@users.noreply.github.com>
2020-11-25 13:34:07 +0000

    Update asset-functions.php

--- php/2020/asset-functions.php
+++ php/2020/asset-functions.php
@@ -9,10 +9,18 @@ if(is_front_page()) {  ?>
 <link rel="stylesheet" href="/wp-content/themes/dante-child/static/css/mgb-homepage.css">
 <?php  }
 };
-
+// LOAD SWIPER SLIDER
 function load_swiper() {
     wp_enqueue_style( 'swiper-slider-css', get_stylesheet_directory_uri() . '/styles/swiper-bundle.min.css',false,'1.1','all');
     wp_enqueue_script( 'swiper-slider-js', get_stylesheet_directory_uri() . '/js/lib/swiper-bundle.min.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'swiper-slider-config-js', get_stylesheet_directory_uri() . '/js/swiper-sliders-config.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'load_swiper' );
+
+// LOAD INSTAGRAM FEED - HOMEPAGE
+function load_insta_feed() {
+	if(is_front_page()) {
+    wp_enqueue_script( 'insta-feed-js', get_stylesheet_directory_uri() . '/js/lib/instagram-feed.js', array(), '1.0.0', true );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'load_insta_feed' );
