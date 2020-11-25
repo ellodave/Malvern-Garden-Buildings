6ae7152e595403aeeb811c098acd46888bd833d8
ellodave <34511073+ellorich@users.noreply.github.com>
2020-11-25 11:45:58 +0000

    Update page-specific-stylesheet.php

--- php/page-specific-stylesheet.php
+++ php/page-specific-stylesheet.php
@@ -1,3 +1,7 @@
+// GLOBAL CSS STYLES
+wp_enqueue_style( 'mgb-global-styles', '/wp-content/themes/dante-child/static/css/mgb-styles.css',false,'1.1','all');
+wp_enqueue_style( 'tailwind-css', '/wp-content/themes/dante-child/static/css/tailwind.css',false,'1.1','all');
+
 // HOMEPAGE SPECIFIC CSS
 add_action('wp_head', 'home_main_css');
 function home_main_css(){
